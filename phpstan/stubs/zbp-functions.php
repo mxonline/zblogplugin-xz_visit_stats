ef5d1184a67da7bec6ea457c22c3f0e5b192a314
明月清风 <chenzhaoyan2003@hotmail.com>
2026-08-24 02:12:59 +0800

    chore: add zblog function stubs for phpstan

---
+++ phpstan/stubs/zbp-functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function CheckCSRFTokenValid(...$args): bool
+{
+    return true;
+}
+
+function GetValueInArrayByCurrent(...$args)
+{
+    return null;
+}
+
+function RegisterPlugin(...$args): void
+{
+}
+
+function Add_Filter_Plugin(...$args): void
+{
+}
+
+function RunTime(...$args)
+{
+    return null;
+}
+
+function GetVars(...$args)
+{
+    return null;
+}
