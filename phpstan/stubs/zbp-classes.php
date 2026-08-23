d076d165a3a81f3e50099d81fbea999274e55260
明月清风 <chenzhaoyan2003@hotmail.com>
2026-08-24 02:32:57 +0800

    fix: improve zblog phpstan class stubs

--- phpstan/stubs/zbp-classes.php
+++ phpstan/stubs/zbp-classes.php
@@ -5,6 +5,11 @@ class PageBar
     public int $Count = 0;
     public int $PageCount = 0;
     public int $PageNow = 0;
+    public int $PageBarCount = 0;
+
+    public function __construct()
+    {
+    }
 
     public function Make(): void
     {
