73afe2ce12f13f58e852f5a0ba05b34d62f95472
明月清风 <chenzhaoyan2003@hotmail.com>
2026-08-24 02:13:02 +0800

    chore: add zblog class stubs for phpstan

---
+++ phpstan/stubs/zbp-classes.php
@@ -0,0 +1,13 @@
+<?php
+
+class PageBar
+{
+    public int $Count = 0;
+    public int $PageCount = 0;
+    public int $PageNow = 0;
+    public int $PageCount = 0;
+
+    public function Make(): void
+    {
+    }
+}
