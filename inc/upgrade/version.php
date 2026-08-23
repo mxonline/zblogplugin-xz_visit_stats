ac25f6799c65a728951b5e86a2531f0d3d293a47
明月清风 <chenzhaoyan2003@hotmail.com>
2026-08-24 06:11:46 +0800

    feat(v2): add upgrade version manager skeleton

---
+++ inc/upgrade/version.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('ZBP_PATH')) {
+    exit('Access denied');
+}
+
+function xz_visit_stats_upgrade_current_version()
+{
+    return '1.3.0';
+}
+
+function xz_visit_stats_upgrade_target_version()
+{
+    return '2.0.0';
+}
