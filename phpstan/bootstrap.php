3e1b4413a402dcbf8f1980b147064b10ece0d8be
明月清风 <chenzhaoyan2003@hotmail.com>
2026-08-24 02:33:06 +0800

    fix: add zblog globals for phpstan

--- phpstan/bootstrap.php
+++ phpstan/bootstrap.php
@@ -2,3 +2,15 @@
 
 require_once __DIR__ . '/stubs/zbp-functions.php';
 require_once __DIR__ . '/stubs/zbp-classes.php';
+
+if (!isset($zbp)) {
+    $zbp = null;
+}
+
+if (!isset($blogpath)) {
+    $blogpath = '';
+}
+
+if (!isset($bloghost)) {
+    $bloghost = '';
+}
